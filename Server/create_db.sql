0ebe444c7058c0054422166b1cc45d2f60414db5
merlin <merlingoulet@live.fr>
2017-01-20 12:54:39 +0100

    Add support for the postgresql database

---
+++ Server/create_db.sql
@@ -0,0 +1,7 @@
+-- SQL files, will be executed at launch.
+-- We need two users, one with rw right and another with just r right
+-- Please julien create them
+-- We need just one database : the Geoloc Database
+-- After that you're free to create you're tables
+--
+--Dont forget to test the existence of the same users and database
